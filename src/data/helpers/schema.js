8da1c29fff564204b334201dc133f53b56262323
jimCresswell <jim.cresswell@gmail.com>
2020-02-18 13:46:29 +0000

    Refactor validateData to avoid mutating error array in place

--- src/data/helpers/schema.js
+++ src/data/helpers/schema.js
@@ -4,7 +4,7 @@
  * @param {*} obj
  * @returns string
  */
-function getType (obj) {
+function _getType (obj) {
   let type = typeof obj
   if (type === 'object' && Array.isArray(obj)) {
     type = 'array'
@@ -13,47 +13,61 @@ function getType (obj) {
 }
 
 /**
- * Check an object against a schema, throw if props or types in the schema are not followed.
- * The data object can contain additional props not in the schema.
+ * Loop over the provided schema and recursively check the data satisfies it.
  *
- * @param Object data The data to be checked against the schema
- * @param Object schema The schema
- * @throws A TypeError is the schema is not followed.
+ * @param {Object} schema The schema to check against.
+ * @param {Object} data The data to check.
+ * @param {string} path the object dot notation path to the current sub-schema from the parent schema.
+ * @returns {Array} An array containing any accumulated failures of the data to meet the schema.
  */
-function validateData (data, schema) {
+function _checkParams (schema, data, path = '') {
   const paramErrors = []
-  // Format error output.
-  paramErrors.toErrorString = function () {
-    return this.reduce(
-      (p, c) => { return (p += c.toString() + '\n') },
-      ''
-    )
-  }
 
   // Note the loop is over the schema, there are no constraints on data properties outside the schema.
   for (const prop in schema) {
     const dataValue = data[prop]
+    const schemaPropType = _getType(schema[prop])
+
     // The data should contain a value for each schema property.
     if (undefined === dataValue) {
-      paramErrors.push(`Required property '${prop}' is undefined.`)
-      // The data should contain a value of the correct type.
+      paramErrors.push(`Required property '${path + prop}' is undefined.`)
+    // The data should contain a value of the correct type.
     } else {
-      const schemaType = getType(schema[prop])
-      const dataType = getType(data[prop])
-      if (schemaType !== dataType) {
-        paramErrors.push(
-          `Expected property '${prop}' to have type '${schemaType}', received value '${data[prop]}' with type '${dataType}'.`
-        )
+      const dataPropType = _getType(data[prop])
+      if (schemaPropType !== dataPropType) {
+        paramErrors.push(`Expected property '${path + prop}' to have type '${schemaPropType}', received value '${data[prop]}' with type '${dataPropType}'.`)
       }
     }
   }
 
+  return paramErrors
+}
+
+// Format error output.
+function _toErrorString (paramErrors) {
+  return paramErrors.reduce(
+    (p, c) => { return (p += c.toString() + '\n') },
+    ''
+  )
+}
+
+/**
+ * Check an object against a schema, throw if props or types in the schema are not followed.
+ * The data object can contain additional props not in the schema.
+ *
+ * @param {Object} data The data to be checked against the schema
+ * @param {Object} schema The schema
+ * @throws {TypeError} A TypeError if the schema is not followed.
+ */
+function validateData (data, schema) {
+  const paramErrors = _checkParams(schema, data)
+
   // If schema errors were found then throw.
   if (paramErrors.length) {
     if (paramErrors.length > 1) {
-      throw new TypeError('Schema errors:\n' + paramErrors.toErrorString())
+      throw new TypeError('Schema errors:\n' + _toErrorString(paramErrors))
     }
-    throw new TypeError('Schema error: ' + paramErrors.toErrorString())
+    throw new TypeError('Schema error: ' + _toErrorString(paramErrors))
   }
 }
 
