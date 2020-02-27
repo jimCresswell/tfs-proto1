9264ae98e0111a0b9098c2c6646560774ba0c021
jimCresswell <jim.cresswell@gmail.com>
2020-02-27 15:10:23 +0000

    Update test.js

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,7 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('Smoke test', () => {
-  it('The app starts and renders', () => {
+  it('The app starts and content renders', () => {
     cy.visit('/')
     cy.contains('h1', 'The Tree Data')
   })
